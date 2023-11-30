2510bc28e8758ba6344d0ebcda56764fc81e107d
somerandombaddeveloper <106110368+somerandombaddeveloper@users.noreply.github.com>
2023-11-30 09:26:44 -0600

    BrowserAction now disabled on pages other than Clever

--- background.js
+++ background.js
@@ -1,7 +1,16 @@
-chrome.runtime.onMessage.addListener(function(message){
-    if (message == "starthack"){
-        chrome.scripting.executeScript({
-            file: 'main.js'
-        });
-    };
-});
+let matchurl = ["://sso.fhsdschools.org/"];
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    for (let i in matches) {
+        if (tab.url.includes(matches[i])){
+            chrome.browserAction.enable(
+                tabId
+            )
+        }
+        else {
+            chrome.browserAction.disable(
+                tabId
+            )
+        }
+    }
+})
