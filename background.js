eecd1d9ee76777e778a8e62d7bfc7e8481a5ed21
Daniel Shepherd <dbubbis@outlook.com>
2023-11-30 17:29:46 -0600

    added some console.log stuff

--- background.js
+++ background.js
@@ -4,6 +4,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     for (let i in matchurl) {
         if (tab.url.includes(matchurl[i])){
             console.log("On clever!")
+            console.log(tabId);
             chrome.browserAction.enable(tabId);
             chrome.browserAction.setIcon(
                 "cleverbomb.png"
@@ -11,6 +12,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
         }
         else {
             console.log("Not on clever!")
+            console.log(tabId);
             chrome.browserAction.disable(tabId);
             chrome.browserAction.setIcon(
                 "cleverbomb-disabled.png"
