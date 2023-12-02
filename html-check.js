91fcd7ea3d61272b4483119a8f311e5ada86e90b
Daniel Shepherd <dbubbis@outlook.com>
2023-12-01 18:16:01 -0600

    Fixed javascript a little

--- html-check.js
+++ html-check.js
@@ -1,4 +1,6 @@
-document.getElementById('hackSubmit').onclick = function(){
+var hacksubmit = document.getElementById('hackSubmit')
+
+hacksubmit.onclick = function(){
     console.log("html-check.js confirmed...")
     chrome.runtime.sendMessage({"pressed": "start"});
 }
