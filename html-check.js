bde8e708077dafe558fdae2b1013d543730138a4
Daniel Shepherd <dbubbis@outlook.com>
2023-12-03 12:31:16 -0600

    Fixed GitHub link

--- html-check.js
+++ html-check.js
@@ -1,6 +1,12 @@
 var hacksubmit = document.getElementById('hackSubmit');
+var link1 = document.getElementById('githublink');
 
 hacksubmit.onclick = function(){
     console.log("html-check.js confirmed...");
     chrome.runtime.sendMessage({"pressed": "start"});
+}
+
+githublink.onclick = function() {
+    console.log("Redirecting...");
+    chrome.tabs.create({url: "https://github.com/somerandombaddeveloper/cleverbomb"})
 }
