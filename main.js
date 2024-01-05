172425ba2ad62586bbbc3a85c5d269346af648db
Daniel Shepherd <dbubbis@outlook.com>
2024-01-05 15:55:51 -0600

    Testing debug stuff

--- main.js
+++ main.js
@@ -32,7 +32,9 @@ var numbertry = 0;
 var numbertrytostr = null;
 for (var numbertry = 0; numbertry <= 999; numbertry++) {
 	console.info("delay");
-	setTimeout(hack, 1000);
+	setTimeout(function () {
+		console.info('settimeout is weird');
+	}, 5000);
 	//breaks if stop button is clicked
 	if (breakHack == "abort") {
 		console.info("Hack aborted.");
